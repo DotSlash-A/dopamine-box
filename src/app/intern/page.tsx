380f5058d12c33f13f75d0e060a5530f44dbae7e
Ananya <91440435+DotSlash-A@users.noreply.github.com>
2024-02-19 16:39:50 +0530

    oof

---
+++ src/app/intern/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="prose mx-auto">
+      <h1>Heading 1</h1>
+      <p>This is a paragraph under Heading 1.</p>
+
+      <h2>Heading 2</h2>
+      <p>This is a paragraph under Heading 2.</p>
+
+      <h3>Heading 3</h3>
+      <p>This is a paragraph under Heading 3.</p>
+
+      {/* Add more content here */}
+    </div>
+  );
+}
